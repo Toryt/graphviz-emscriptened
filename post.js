04a72f3ca71132964d7f6bf43d12765ca15b5f9b
Jan Dockx <jandockx@gmail.com>
2015-03-08 22:48:41 +0100

    The original emscripten does a better job for node that this attempt by viz.js. The print method should be overridden. Otherwise we get too much output on the console.

--- post.js
+++
@@ -1,2 +0,0 @@
-  return Module["ccall"]("vizRenderFromString", "string", ["string", "string", "string", "string"], [src, format, engine, options]);
-}
--- pre.js
+++ pre.js
@@ -1,18 +1,5 @@
-window["Viz"] = function(src, format, engine, options) {
-  if (typeof(format) === 'undefined') {
-    format = 'svg';
-  }
-  if (typeof(engine) === 'undefined') {
-    engine = 'dot';
-  }
-  if (Array.isArray(options)) {
-    options = options.join('|');
-  } else {
-    options = null;
-  }
-
-  var Module = {};
-  Module["return"] = "";
-  Module["print"] = function(text) {
-    Module["return"] += text + "\n";
-  }
+var Module = {};
+Module["return"] = "";
+Module["print"] = function(text) {
+  Module["return"] += text + "\n";
+}
